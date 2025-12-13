974227f9f3bd2d2da33959d6c743f1b2fbdb3a67
Madison (Pfaff) Edgar <7844510+madiedgar@users.noreply.github.com>
2025-12-13 20:32:20 +0400

    clean: remove site template

--- src/lib/constants.ts
+++
@@ -1,3 +0,0 @@
-export const SITE_TITLE = "A Christmas Ancestry";
-export const AMAZON_LINK = "https://amazon.com/placeholder"; // TODO: Update with real Amazon link
-export const EMAIL_FROM = "A Christmas Ancestry <noreply@achristmasancestry.com>";
